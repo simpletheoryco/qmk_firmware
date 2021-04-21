8fe48067d05f78342e259bbde1e7c7a3ae267950
Tam Truong <design@tamtruong.com>
2021-04-20 21:22:48 -0500

    adds test rig config with ws2812

---
+++ keyboards/zammykoo/streamcheap/kb.h
@@ -0,0 +1,12 @@
+#ifndef KB_H
+#define KB_H
+
+#include "quantum.h"
+
+#define KEYMAP( \
+	K000, K001, K002, K003, K004, K005, K006, K007, K008, K009  \
+) { \
+	{ K000,  K001,  K002,  K003,  K004,  K005,  K006,  K007,  K008,  K009 }  \
+}
+
+#endif
